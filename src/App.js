1afd935320dc50f04e152dc09be0b8b07ee84fd1
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2022-10-24 12:23:09 +0800

    feat(init): react-template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routerConfig from './config/router';
+
+import './App.css';
+
+const router = createBrowserRouter(routerConfig, {basename: '/react-template'});
+
+export default function App() {
+  return (<React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+  );
+}
---
+++ src/config/router.js
@@ -0,0 +1,10 @@
+import HomePage from '../pages/Home'
+
+const routerConfig =  [
+    {
+        path: '/',
+        element: <HomePage />,
+    }
+];
+
+export default routerConfig;
